9999ea5ec557f3bc01d221d601c04e2b24570451
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2022-09-28 14:34:16 +0100

    Generate in quickstatements v1 format

--- step-three.py
+++ step-three.py
@@ -21,38 +21,54 @@ def main():
         next(gf)
         for line in gf:
             values = line.split("\t")
-
             pmcid = int(values[2])
             pmid = values[3]
             doiid = str(values[4])
 
+            software = values[9]
             curated = values[12]
-            software = values[13].strip()
+            mapped_software = values[13].strip()
 
             #     software 7094578
             # not software 7675631
             # if curated == "software":
-            if software in software_series.index:
+            if mapped_software in software_series.index:
+                found = []
+
                 if pmid:
                     pmid = int(pmid[:-2])  # remove .0
                     if pmid in pm_series.index:
-                        data.append([pm_series[pmid], software_series[software]])
+                        found = [pm_series[pmid], software_series[mapped_software]]
                 elif pmcid in pmc_series.index:
-                    data.append([pmc_series[pmcid], software_series[software]])
+                    found = [pmc_series[pmcid], software_series[mapped_software]]
                 elif doiid in doi_series.index:
-                    data.append([doi_series[doiid], software_series[software]])
+                    found = [doi_series[doiid], software_series[mapped_software]]
 
+                if found and software:
+                    found.append(software)
+                if found:
+                    data.append(found)
+
+                    # if len(data) > 0:
+                    #     print(values)
+                    #     print(data)
+                    #     exit()
                     # i += 1
-                    # if i > 20:
+                    # if i > 1000:
                     #     break
 
     writeData(data)
 
 
 def writeData(data):
-    with open("triples-all.csv", "w") as w:
+    with open("qsv1-all.csv", "w") as w:
         for value in data:
-            w.write(value[0] + ",P4510," + value[1] + "\n")
+            if len(value) == 2:
+                w.write(value[0] + "|P4510|" + value[1] + "|S248|Q114078827\n")
+            else:
+                w.write(
+                    f'{value[0]}|P4510|{value[1]}|P1932|"{value[2]}"|S248|Q114078827\n'
+                )
 
 
 if __name__ == "__main__":
