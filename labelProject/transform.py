d1191d565d428c475ee5c512098d4ca3c7efd3c9
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2022-10-16 13:48:58 +0100

    Update processing to handle quoting properly

--- labelProject/transform.py
+++ labelProject/transform.py
@@ -1,5 +1,5 @@
 import time
-
+import csv
 import pandas as pd
 import html
 
@@ -7,18 +7,13 @@ import html
 def main():
     df = pd.read_csv("quarry-68091.csv")
 
-    # To add a label in a specific language to an item, use "Lxx" instead of a property, with "xx" as the language code.
-    # Example: Q340122 TAB Lpl TAB "Cyprian Kamil Norwid"
-    # Meaning: add Polish label "Cyprian Kamil Norwid" to Cyprian Norwid (Q340122)
+    # Perform multiple times as strings can be encoded multiple times
+    for _ in range(10):
+        df['text'] =  df['text'].apply(lambda x: html.unescape(x))
 
-    df['id'] = 'Q' + df['id'].astype(str)
-    df['language'] = 'L' + df['language'].astype(str)
-    df['text'] =  df['text'].apply(lambda x: html.unescape(x))
-    # 2.3k of these have been encoded twice so transform it again
-    df['text'] =  df['text'].apply(lambda x: html.unescape(x))
-    # df['text'] =  df['text'].apply(lambda x: '"' + x + '"')
+    df['text'] =  df['text'].apply(lambda x: '"' + x + '"')
 
-    df.to_csv("quickstatements.tsv", sep="\t", index=False, header=False)
+    df.to_csv("quickstatements.tsv", sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE)
 
 
 def timer(tick, msg=""):
